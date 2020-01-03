16eb80df193285d12572cdc450904348fa33eb97
saaadalishah <sashsaad@gmail.com>
2020-01-03 18:37:49 +0500

    first test

--- src/Tests/App.test.js
+++ src/Tests/App.test.js
@@ -16,17 +16,3 @@ test("test homepage url", async () => {
     const url = await page.url();
     assert(url === 'http://localhost:3000/');
 })
-
-// import puppeteer from 'puppeteer';
-
-// Otest('h1 loads correctly', async () => {
-//     let browser = await puppeteer.launch({
-//         headless: false
-//     })
-//     let page = await browser.newpage()
-
-//     await page.goto('http://localhost:3000/')
-//     const html = await page.$eval('p.para1', e => e.innerHtml)
-//     expect(html).toBe('PEACE OF MIND, WHEN THEY NEED IT MOST')
-//     browser.close()
-// });
