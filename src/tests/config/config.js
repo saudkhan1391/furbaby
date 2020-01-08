2aa4eb9afc08956c2a8404088ab87ca517ec0f67
assadakramiintellect <assad@iintellect.co.uk>
2020-01-08 15:56:01 +0500

    more fixes

--- src/tests/config/config.js
+++ src/tests/config/config.js
@@ -1,6 +1,6 @@
 
 module.exports ={
- path:'https://fbt-desktop-integration-testing.netlify.com//',
+ path:'https://fbt-desktop-integration-testing.netlify.com/',
  email:'engineering@redsqware.com',
  pass:'furbaby123'
 
