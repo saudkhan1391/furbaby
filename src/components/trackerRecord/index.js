71336abe3ec780d313271fad4b8dc11bfe8fe726
assadakramiintellect <assad@iintellect.co.uk>
2020-01-22 19:18:11 +0500

    detail page image fixed

--- src/components/trackerRecord/index.js
+++ src/components/trackerRecord/index.js
@@ -18,11 +18,15 @@ const trackerRecord = (props) => {
             data.appointmentId = current.appointmentId;
             setFurBaby(data);
         });
+        firebase.database().ref("/pets/"+current.petId+"/coverPhoto").on('value', (snapshot) => {
+            setCoverPhoto(snapshot.val());
+        });
         setShow(true);
     }, [current]);
     let { trackingComponent, medications, food, pee, notes, appointmentType, petOwnerNote, startTime, galleryPhotos, description, staffNotes } = furBaby;
     let tracker =  trackingComponent ? JSON.parse(trackingComponent) : [];
-    let { name, coverPhoto } = current.pet;
+    let { name, coverPhoto: photo } = current.pet;
+    const [coverPhoto, setCoverPhoto] = useState(photo);
     let { firstName, lastName, workPhone, email } = current.petOwner;
     medications = medications ? JSON.parse(medications) : [];
     food = food ? JSON.parse(food) : [];
