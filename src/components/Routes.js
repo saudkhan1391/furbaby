784683a5b5f76c00d74be864e47e1d34c3f5af4a
assadakramiintellect <assad@iintellect.co.uk>
2020-03-11 13:14:41 +0500

    hooks trigger

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -132,7 +132,17 @@ const Routes = (props) => {
                     payload: data
                 })
             });
-            firebase.database().ref("/appointments").orderByChild('clinicId').equalTo(clinicId).on('value', (snapshot) => {
+            firebase.database().ref("/appointments").limitToLast(1).orderByChild('clinicId').equalTo(clinicId).on('child_added', (snapshot) => {
+                console.log("child added", snapshot.val());
+                if(take){
+                    getClinicData(id, false);
+                }else {
+                    take = true;
+                }
+            });
+
+            firebase.database().ref("/appointments").orderByChild('clinicId').equalTo(clinicId).on('child_changed', (snapshot) => {
+                console.log("child changed", snapshot.val());
                 if(take){
                     getClinicData(id, false);
                 }else {
